64944d5e22590c8d0a8bfdf092bee47c136543b3
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-26 13:20:34 -0700

    add substantial styling

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -11,28 +11,39 @@ import { EditFoodDetailsComponent } from './edit-food-details.component';
   pipes: [CaloricAssessmentPipe],
   directives: [FoodComponent, NewFoodComponent, EditFoodDetailsComponent],
   template: `
-  <label>Filter by Calories (Default is 500):
-    <input id="cal-input" class="input-field" value="500" #calorieFilterAmt/>
-    <select class="input-field" #selectValue>
-      <option selected value="all">Show All</option>
-      <option value="under">Under</option>
-      <option value="over">Over</option>
-    </select>
-    <button class="btn filter-button" (click)="onFilterClick(selectValue)">Filter</button>
-  </label>
-  <div class="row header-row">
-    <div class="col-xs-3"><h3>Date</h3></div>
-    <div class="col-xs-3"><h3>Name</h3></div>
-    <div class="col-xs-2"><h3>Calories</h3></div>
-    <div class="col-xs-4"><h3>Description</h3></div>
+  <div class="padding-box">
+    <div class="top-wrapper row">
+      <div class="col-sm-5">
+        <p>Filter by Calories (Default is 500)</p>
+        <input id="cal-input" class="input-field" value="500" #calorieFilterAmt/>
+      </div>
+      <div class="col-sm-4">
+        <p>Choose Parameter</p>
+        <select class="input-field" #selectValue>
+          <option selected value="all">Show All</option>
+          <option value="under">Under</option>
+          <option value="over">Over</option>
+        </select>
+      </div>
+      <div class="col-sm-3">
+        <p>Apply Filter:</p>
+        <button class="btn filter-button" (click)="onFilterClick(selectValue)">Filter</button>
+      </div>
+    </div>
+    <div class="row header-row">
+      <div class="col-xs-3"><h3>Date</h3></div>
+      <div class="col-xs-3"><h3>Name</h3></div>
+      <div class="col-xs-2"><h3>Calories</h3></div>
+      <div class="col-xs-4"><h3>Description</h3></div>
+    </div>
+    <food-display *ngFor="#currentFood of foodList | caloric: threshold:calorieFilterAmt"
+      (click)="foodClicked(currentFood)"
+      [class.selected]="currentFood === selectedFood"
+      [food]="currentFood">
+    </food-display>
+    <edit-food-details *ngIf="selectedFood" [food]="selectedFood"></edit-food-details>
+    <new-food (onSubmitNewFood)="createFood($event)"></new-food>
   </div>
-  <food-display *ngFor="#currentFood of foodList | caloric: threshold:calorieFilterAmt"
-    (click)="foodClicked(currentFood)"
-    [class.selected]="currentFood === selectedFood"
-    [food]="currentFood">
-  </food-display>
-  <edit-food-details *ngIf="selectedFood" [food]="selectedFood"></edit-food-details>
-  <new-food (onSubmitNewFood)="createFood($event)"></new-food>
   `
 })
 export class FoodListComponent {
--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -6,14 +6,18 @@ import { Food } from './food.model';
   selector: 'new-food',
   outputs: ['onSubmitNewFood'],
   template: `
-    <div class='food-form'>
-      <div class="row form-row">
-        <h3>Create Food:</h3>
-        <input placeholder="Name" class="col-sm-4 input-field" #newName>
-        <input placeholder="Calories" class="col-sm-4 input-field" #newCalories>
-        <input placeholder="Description" class="col-sm-4 input-field" #newDescription>
-        <button (click)="addFood(newName, newCalories, newDescription)" class="btn add-button">Add</button>
+    <div class="row form-row">
+      <h3>Create Food:</h3>
+      <div class="col-sm-4">
+        <input placeholder="Name" class="input-field" #newName>
       </div>
+      <div class="col-sm-4">
+        <input type="number" placeholder="Calories" class="input-field" #newCalories>
+      </div>
+      <div class="col-sm-4">
+        <input placeholder="Description" class="input-field" #newDescription>
+      </div>
+      <button (click)="addFood(newName, newCalories, newDescription)" class="btn add-button">Add</button>
     </div>
   `
 })
