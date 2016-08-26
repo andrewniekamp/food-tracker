f72f9baef9c5c03fea789e6d40bb7ff043e3fd3e
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-25 17:37:16 -0700

    add initial template files

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Teh bestorz epp!!1</h1>
+    </div>
+  `
+})
+export class AppComponent {
+
+}
