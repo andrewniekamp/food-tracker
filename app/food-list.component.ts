395f85d0ae9fdf064f978b8318274262582739cf
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-26 09:24:38 -0700

    add caloric assessment pipe to food list component

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -2,12 +2,21 @@ import { Component } from 'angular2/core';
 import { FoodComponent } from './food.component';
 import { Food } from './food.model';
 import { NewFoodComponent } from './new-food.component'
+import { CaloricAssessmentPipe } from './caloric-assessment.pipe';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
+  pipes: [CaloricAssessmentPipe],
   directives: [FoodComponent, NewFoodComponent],
   template: `
+  <label>Filter by Calories:
+    <select (change)="onChange($event.target.value)" class="filter">
+      <option selected value="all">Show All</option>
+      <option value="under">Under</option>
+      <option value="over">Over</option>
+    </select>
+  </label>
   <div class="row header-row">
     <div class="col-xs-4"><h3>Name</h3></div>
     <div class="col-xs-4"><h3>Calories</h3></div>
