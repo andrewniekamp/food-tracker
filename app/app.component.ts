40e6285087fb9cc432dd980b316441ecb91ce44c
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-08-25 17:50:18 -0700

    add app component details to receive list from list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from 'angular2/core';
+import { Food } from './food.model'
 
 @Component({
   selector: 'my-app',
   template: `
-    <div class="container">
-      <h1>Teh bestorz epp!!1</h1>
-    </div>
+  <div class="container-fluid header">
+    <img id="header-img" src="">
+    <h1>Food tracker app</h1>
+  </div>
+  <div class="container">
+    <food-list
+      [foodList]="foods"
+      (onFoodSelect)="foodWasSelected($event)">
+    </food-list>
+  </div>
   `
 })
 export class AppComponent {
-
+  public food: Food[];
+  constructor(){
+    this.food = [
+      new Food("Dragon's Blood Vermillion Ale", 8, "6.5%"),
+      new Food("Excalibur", 9, "7%"),
+      new Food("Griffin Sweat Pale Ale", 1, "14.5%"),
+      new Food("Eye of Newt Brew", 5, "12.5%"),
+      new Food("Shady Brownie Ale", 2, "3.5%")
+    ];
+  }
 }
---
+++ app/food.component.ts
@@ -0,0 +1,18 @@
+// import { Component } from 'angular2/core';
+// import { Food } from './food.model'
+//
+// @Component({
+//   selector: 'food-display',
+//   inputs: ['food'],
+//   template: `
+//     <div class="row content">
+//       <div class="col-sm-3">Name: {{ food.name }}</div>
+//       <div class="col-sm-3">Brand: {{ food.calories }}</div>
+//       <div class="col-sm-1">Price: {{ food.description }}</div>
+//       <div class="col-sm-3" *ngIf="keg.pints > 0"><button (click)="keg.pints = keg.pints -1" class="btn btn-warning">Serve Pint</button></div>
+//     </div>
+//   `
+// })
+// export class FoodComponent {
+//   public food: Food;
+// }
